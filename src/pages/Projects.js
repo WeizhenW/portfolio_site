87c8de639cccbb73837a35ea5c4a95ff818f058b
Weizhen Wang <wangwzhen@gmail.com>
2019-07-21 18:43:23 -0500

    update project page and add link to github repo

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Layout from '../components/Layout';
 
@@ -9,123 +9,140 @@ import feedbackForm from '../assets/images/feedback-form.png';
 
 //material ui
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 
-const IndexPage = () => (
-    <Layout fullMenu>
-        <article id="main">
-            <header>
-                <h2>Projects List</h2>
-                <p>A full list of all my projects</p>
-            </header>
+const styles = {
+    media: {
+        height: "350px",
+		boxShadow: "3px 4px 10px -5px grey",
+      },
+}
 
-            <section className="wrapper style5">
-                <div className="inner">
-                    <section className="spotlight">
-                        <div className="image">
-                            <a href='https://movie-master-2019.herokuapp.com/#/' target="_blank">
-                                <img src={movieMaster} alt="" class="oneImage" />
-                                {/* <div className="middle">
-                <Grid container className="text">
-                    <Grid item xs={4}><p className="techUsed">ReactJS</p></Grid>
-                    <Grid item xs={4}><p className="techUsed">Redux</p></Grid>
-                    <Grid item xs={4}><p className="techUsed">Saga</p></Grid>
-                    <Grid item xs={4}><p className="techUsed">Node.js</p></Grid>
-                    <Grid item xs={4}><p className="techUsed">Express</p></Grid>
-                    <Grid item xs={4}><p className="techUsed">PostgreSQL</p></Grid>
-                    <Grid item xs={12}><p className="techUsed">Material-UI</p></Grid>
-                </Grid>
-                </div> */}
-                            </a>
-                        </div>
-                        <div className="content">
-                            <h2>
-                                Movie Master
-                <br />
-                            </h2>
-                            <p>
-                                This is a movie database app with full read/edit/search/delete functionalities and it also provides the search functionality
-                                at the public open movie database by using OMDB API
-            </p>
-                        </div>
+class IndexPage extends Component {
+    render() {
+        return (
+
+            <Layout fullMenu>
+                <article id="main">
+                    <header>
+                        <h2>Projects Page</h2>
+                        {/* <p>A full list of my work</p> */}
+                    </header>
+
+                    <section className="wrapper style5">
+                        <Grid container spacing={3} className="inner">
+                            <Grid item xs={12} sm={6}>
+                                <Card>
+                                    <CardMedia className="image" >
+                                        <img src={movieMaster} alt="" style={styles.media}/>
+                                    </CardMedia>
+                                    <CardContent className="content">
+                                        <h3>
+                                            Movie Master
+                                        <br />
+                                        </h3>
+                                        <p>
+                                            This is a movie database app with full read, edit, search and delete functionalities and it also provides the search functionality
+                                            at the public open movie database by using OMDB API
+                                        </p>
+                                    </CardContent>
+                                    <CardActions>
+                                        <a href='https://movie-master-2019.herokuapp.com/#/' target="_blank">
+                                            <Button variant="outlined" color="primary">View Site</Button>
+                                        </a>
+                                        <a href="https://github.com/WeizhenW/Movie-Master" target="_blank">
+                                            <Button variant="outlined" color="secondary">View Code</Button>
+                                        </a>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <Card>
+                                    <CardMedia className="image" >
+                                        <img src={toDo} alt="to do" style={styles.media} />
+                                    </CardMedia>
+                                    <CardContent className="content">
+                                        <h3>
+                                            To Do List
+                                        <br />
+                                        </h3>
+                                        <p>
+                                            This is a jQuery app to track the to-dos. Functionalities include add/delete tasks, mark tasks as completed, show overdue tasks and sort the list.
+                                        </p>
+                                    </CardContent>
+                                    <CardActions>
+                                        <a href='https://to-do-wz.herokuapp.com/' target="_blank">
+                                            <Button variant="outlined" color="primary">View Site</Button>
+                                        </a>
+                                        <a href="https://github.com/WeizhenW/To-Do-List" target="_blank">
+                                            <Button variant="outlined" color="secondary">View Code</Button>
+                                        </a>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <Card>
+                                    <CardMedia className="image" >
+                                        <img src={jobTracker} alt="job tracker" style={styles.media} />
+                                    </CardMedia>
+                                    <CardContent className="content">
+                                        <h3>
+                                            Job Cracker App
+                                        <br />
+                                        </h3>
+                                        <p>
+                                            This app aims to help centralizing the job application related information at a single location
+                                            and ease the application tracking progress for job seekers
+                                        </p>
+                                    </CardContent>
+                                    <CardActions>
+                                        <a href='https://job-cracker.herokuapp.com' target="_blank">
+                                            <Button variant="outlined" color="primary">View Site</Button>
+                                        </a>
+                                        <a href="https://github.com/WeizhenW/Job-Tracker-App" target="_blank">
+                                            <Button variant="outlined" color="secondary">View Code</Button>
+                                        </a>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <Card>
+                                    <CardMedia className="image" >
+                                        <img src={feedbackForm} alt="feedback form" style={styles.media}/>
+                                    </CardMedia>
+                                    <CardContent className="content">
+                                        <h3>
+                                            Daily Feedback Form
+                                        <br />
+                                        </h3>
+                                        <p>
+                                        This is a React based app where user can input their daily feedback and submit to admin.
+                                        It also has an admin view to check and manage the response dashboard.
+                                        </p>
+                                </CardContent>
+                                <CardActions>
+                                        <a href='https://student-feedback-form.herokuapp.com' target="_blank">
+                                            <Button variant="outlined" color="primary">View Site</Button>
+                                        </a>
+                                        <a href="https://github.com/WeizhenW/Student-Feedback_Form" target="_blank">
+                                            <Button variant="outlined" color="secondary">View Code</Button>
+                                        </a>
+                                    </CardActions>
+                            </Card>
+                            </Grid>
+                        </Grid>
                     </section>
-
-                    <section className="spotlight">
-                        <div className="image">
-                            <a href="https://to-do-wz.herokuapp.com/" target="_blank">
-                                <img src={toDo} alt="" className="oneImage" />
-                            </a>
-                        </div>
-                        <div className="content">
-                            <h2>
-                                To Do List
-            <br />
-                            </h2>
-                            <p>
-                                jQuery app to track the to-dos. Functionalities include add/delete tasks, mark tasks as completed, show overdue tasks and sort the list.
-          </p>
-                        </div>
-                    </section>
-
-                    <section className="spotlight">
-                        <div className="image">
-                            <a href='https://job-cracker.herokuapp.com/' target="_blank">
-                                <img src={jobTracker} alt="" className="oneImage" />
-                                {/* <div className="middle">
-              <Grid container className="text">
-                <Grid item xs={4}><p className="techUsed">ReactJS</p></Grid>
-                <Grid item xs={4}><p className="techUsed">Redux</p></Grid>
-                <Grid item xs={4}><p className="techUsed">Saga</p></Grid>
-                <Grid item xs={4}><p className="techUsed">Node.js</p></Grid>
-                <Grid item xs={4}><p className="techUsed">Express</p></Grid>
-                <Grid item xs={4}><p className="techUsed">PostgreSQL</p></Grid>
-                <Grid item xs={6}><p className="techUsed">Material-UI</p></Grid>
-                <Grid item xs={6}><p className="techUsed">AWS S3</p></Grid>
-              </Grid>
-            </div> */}
-                            </a>
-                        </div>
-                        <div className="content">
-                            <h2>
-                                Job Cracker App
-            <br />
-                            </h2>
-                            <p>
-                                This app aims to help centralizing the job application related information at a single location
-                                and ease the application tracking progress for job seekers
-          </p>
-                        </div>
-                    </section>
-
-                    <section className="spotlight">
-                        <div className="image">
-                            <a href="https://student-feedback-form.herokuapp.com/#/" target="_blank">
-                                <img src={feedbackForm} alt="feedback form" className="oneImage" />
-                                {/* <div className="middle">
-              <Grid container className="text">
-                <Grid item xs={6}><p className="techUsed">ReactJS</p></Grid>
-                <Grid item xs={6}><p className="techUsed">Node.js</p></Grid>
-                <Grid item xs={6}><p className="techUsed">Express</p></Grid>
-                <Grid item xs={6}><p className="techUsed">PostgreSQL</p></Grid>
-                <Grid item xs={12}><p className="techUsed">Material-UI</p></Grid>
-              </Grid>
-            </div> */}
-                            </a>
-                        </div>
-                        <div className="content">
-                            <h2>
-                                Daily Feedback Form
-                            <br />
-                            </h2>
-                            <p>
-                                This is a React based app where user can input their daily feedback and submit to admin.
-                                It also has an admin view to check and manage the response dashboard.
-                            </p>
-                        </div>
-                    </section>
-                </div>
-            </section>
-        </article>
-    </Layout>
-);
+                </article>
+            </Layout>
+        )
+    }
+};
 
 export default IndexPage;
